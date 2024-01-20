7c00c09dc41ac375dcdc5ce8a032929c0283f4f5
samiullah <sami606713@gmail.com>
2024-01-21 01:49:48 +0500

    adding notbook foldere

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 # Error Message Detail
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -18,3 +18,10 @@ class Coustom_exception(Exception):
 
     def __str__(self):
         return self.error_message
+
+# if __name__=="__main__":
+#     try:
+#         n=samillah
+#     except Exception as e:
+#         logging.info("Exception occur")
+#         raise Coustom_exception(e,sys)
